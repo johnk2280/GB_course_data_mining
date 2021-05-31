8b7841a0c3594a5caf056d1e16ac652d7667281f
johnk2280 <johnk2280@gmail.com>
2021-05-31 11:47:49 +0500

    [add] add solution

--- lesson_2_hw/lesson_2_hw.py
+++ lesson_2_hw/lesson_2_hw.py
@@ -5,6 +5,7 @@ from urllib.parse import urljoin
 import bs4
 import lxml
 import pymongo
+from datetime import datetime
 
 
 class GbBlogParse:
@@ -21,17 +22,19 @@ class GbBlogParse:
         self.tasks = []
         self.tasks_creator({self.start_url, }, self.parse_feed)
 
-    def _get_response(self, url):
+    def _get_response(self, url, params=None):
         while True:
             next_time = self.__parse_time + self.delay
             if next_time > time.time():
                 time.sleep(next_time - time.time())
 
-            response = requests.get(url, headers=self.headers)
+            response = requests.get(url, params=params, headers=self.headers)
             print(f'RESPONSE: {response.url}')
             self.__parse_time = time.time()
             if response.status_code == 200:
                 return response
+
+            return f'Something went wrong', response.status_code
 
     def get_task(self, url, callback: typing.Callable) -> typing.Callable:
         def task():
@@ -75,15 +78,50 @@ class GbBlogParse:
     def parse_post(self, response: requests.Response):
         soup = bs4.BeautifulSoup(response.text, 'lxml')
         author_name_tag = soup.find('div', attrs={'itemprop': 'author'})
+        try:
+            img_link = soup.find('div', attrs={'class': 'blogpost-content'}).find('img').get('src')
+        except AttributeError:
+            img_link = 'No img link'
+
+        post_date = datetime.strptime(
+            soup.find('div', attrs={'class': 'blogpost-date-views'}).find('time').get('datetime'),
+            '%Y-%m-%dT%H:%M:%S%z'
+        )
+        comments = self._get_comments(soup.find('comments'))
+
         data = {
             'url': response.url,
             'title': soup.find('h1', attrs={'class': 'blogpost-title'}).text,
             'author': {
                 'url': urljoin(response.url, author_name_tag.parent.attrs['href']),
                 'name': author_name_tag.text
-            }
+            },
+            'img_link': img_link,
+            'post_date': post_date,
+            'comments': comments
         }
         self._save_data(data)
+
+    def _get_comments(self, tag: bs4.element.Tag) -> list:
+        result = []
+        if tag:
+            params = {
+                'commentable_type': tag.get('commentable-type'),
+                'commentable_id': tag.get('commentable-id'),
+                'order': tag.get('order')
+            }
+            url = 'https://gb.ru/api/v2/comments'
+            response = self._get_response(url, params=params)
+            comments = response.json()
+            for comment in comments:
+                data = {
+                    'comment_author': comment['comment']['user'].get('full_name'),
+                    'comment_body': comment['comment'].get('body'),
+                    'comment_children': comment['comment'].get('children')
+                }
+                result.append(data)
+
+            return result
 
     def _save_data(self, data: dict):
         collection = self.db['gb_blog_parse']
